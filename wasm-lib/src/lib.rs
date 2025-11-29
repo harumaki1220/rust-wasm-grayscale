92fa9c4ac90ed8af23d263f6e8c78ca1871671ce
harumaki1220 <haruma181220@gmail.com>
2025-11-30 02:35:09 +0900

    feat: 画像を白黒変換するRustのロジックを実装

--- wasm-lib/src/lib.rs
+++ wasm-lib/src/lib.rs
@@ -1,14 +1,16 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use wasm_bindgen::prelude::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[wasm_bindgen]
+pub fn convert_to_grayscale(data: &mut [u8], _width: u32, _height: u32) {
+    for pixel in data.chunks_mut(4) {
+        let r = pixel[0] as f32;
+        let g = pixel[1] as f32;
+        let b = pixel[2] as f32;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let gray = (0.299 * r + 0.587 * g + 0.114 * b) as u8;
+
+        pixel[0] = gray; // R
+        pixel[1] = gray; // G
+        pixel[2] = gray; // B
     }
 }
